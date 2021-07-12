45fdea7fd55b1d26f4b6bfa938bf818c6993a31a
Cesar Adrian Leyva Luna <72633350+BookmanTasty@users.noreply.github.com>
2021-07-12 17:50:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ include("funciones/funciones.php");
 
 <div class="container-fluid ">
 <div class="row justify-content-center"  >
-<div id="carouselExampleInterval" class="carousel slide justify-content-center" data-bs-ride="carousel" style="width: 70%;">
+<div id="carouselExampleInterval" class=" container-md carousel slide justify-content-center w-75 " data-bs-ride="carousel" >
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="10000">
       <img src="img/baners/4582296.png" class="d-block w-100" alt="...">
